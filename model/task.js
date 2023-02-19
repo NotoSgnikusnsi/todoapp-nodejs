c89b730e0a12ee6d2f2328ba492201e637e0e9cf
Noto <notosgnikusnsi@gmail.com>
2023-02-19 23:47:02 +0900

    update : create taskSchema and userSchema

--- model/task.js
+++ model/task.js
@@ -4,9 +4,36 @@ const taskSchema = new mongoose.Schema({
   name: {
     type: String,
     require: true, // 必須条件
-    trim: true // 空白削除 
+    trim: true, // 空白削除 
+    min: 1,
+    max: 50
+  },
+  check: {
+    type: Boolean,
+    require: true
   }
 });
-let Task = mongoose.model("Task", taskSchema);
 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    unique: true,
+    min: 1,
+    max: 32,
+    trim: true
+  },
+  password: {
+    type: String,
+    require: true,
+    min: 6,
+    max: 100,
+    trim: true
+  }
+})
+
+let Task = mongoose.model("Task", taskSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports = User;
 module.exports = Task;
